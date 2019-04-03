2b2be658631724ef93fc04e39bbc5fad62f0dc37
Plawan Ghimire <44517345+plawan926@users.noreply.github.com>
2019-04-03 20:26:09 +0545

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import calendar
+
+
+
+print(calendar.calendar(2019))
+print()
+
+how_many_leap_days = calendar.leapdays(0000,10000)
+print("Leap Days between 0000 AD to 10000 AD")
+print(how_many_leap_days)
